ea84c510d32803200296d153b9daff2178b3b839
bouabbadi abdessamad <bouabbadiabdessamad@gmail.com>
2025-01-01 22:25:05 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H 
 
 # include <stdlib.h>
 # include <unistd.h>
