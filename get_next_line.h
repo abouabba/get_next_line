37c6d7c1d2679e41f7606e67c519084a1198acae
bouabbadi abdessamad <bouabbadiabdessamad@gmail.com>
2025-01-05 10:56:09 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -11,11 +11,11 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H 
+# define GET_NEXT_LINE_H
 
-# include <stdlib.h> 
+# include <stdlib.h>
 # include <unistd.h>
-# include <limits.h> 
+# include <limits.h>
  
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42   
